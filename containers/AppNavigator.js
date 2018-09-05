d26200be3e934d5e6565a24fc6df58393efa4aa4
Imran Rafiq <ratherimran99@gmail.com>
2018-09-05 15:21:49 +0530

    Using Redux Saga including middleware. First Commit

---
+++ containers/AppNavigator.js
@@ -0,0 +1,103 @@
+import { createStackNavigator, createBottomTabNavigator } from "react-navigation";
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware
+} from "react-navigation-redux-helpers";
+import { connect } from "react-redux";
+import React from "react";
+
+import ProductDetail from "./ProductDetail";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons"
+import ProductListWithFlatList from "./ProductListWithFlatList";
+
+const ListStack = createStackNavigator(
+  {
+    List: {
+      screen: ProductListWithFlatList
+    },
+    Detail: {
+      screen: ProductDetail
+    }
+  },
+  {
+    initialRouteName: "List",
+    navigationOptions: {
+      title: "Product Manager",
+      headerStyle: {
+        backgroundColor: "#00ff80"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+        textAlign: "center"
+      }
+    }
+  }
+);
+
+
+
+export const AppNavigator = createBottomTabNavigator(
+  {
+    List: ListStack,
+    Search: ListStack,
+    
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "List") {
+          iconName = `ios-list-box${focused ? "" : "-outline"}`;
+        } else if (routeName === "Add") {
+          iconName = `ios-add-circle${focused ? "" : "-outline"}`;
+        } else if (routeName === "Stores") {
+          return (
+            <MaterialIcons
+              name="local-grocery-store"
+              size={25}
+              color={tintColor}
+            />
+          );
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "#00ff80",
+      inactiveTintColor: "gray"
+    }
+  }
+);
+
+export const middleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.navState
+);
+const addListener = createReduxBoundAddListener("root");
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator
+        navigation={{
+          dispatch: this.props.dispatch,
+          state: this.props.navState,
+          addListener
+        }}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  navState: state.navState
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+export default AppWithNavigationState;
